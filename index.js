9c2c356aa6ab6539c036755b73bf4b0de23cd71f
admin <admin@Mphos-MacBook-Pro.local>
2022-10-20 19:07:25 +0200

    svg node

--- index.js
+++ index.js
@@ -40,7 +40,25 @@ const y = d3.scaleLinear()
             .attr('y', (d) => y(d.score))
             .attr('height', d => y(0) - y(d.score))
             .attr('width', x.bandwidth())
+            .attr('class','rectangle' )
+
+    function xAxis(g){
+        g.attr('transform', `translate(0, ${height - margin.bottom})`)
+        .call(d3.axisBottom(x).tickFormat(i => data[i].name))
+        .attr('font-size', '20px')
+
+    };
+
+    function yAxis(g){
+        g.attr('transform', `translate(${margin.left}, 0)`)
+        .call(d3.axisLeft(y).ticks(null, data.format))
+
+    }
     
+    svg.append('g').call(xAxis); //append x labs 
+    svg.append('g').call(yAxis); //append x labs 
+
+
     //drawing barries 
     svg.node();
 
